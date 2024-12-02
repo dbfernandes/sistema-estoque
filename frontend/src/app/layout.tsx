d442e4b4613297673f76a4d1054959a6c3a559c0
lazaro <marqueslazaro13@gmail.com>
2024-12-02 09:59:39 -0400

    adicionando o react query ao projeto

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "@/components/Navbar/Navbar";
+import Navbar from "@/components/Navbar/NavBar";
 import BootstrapClient from "@/components/BootstrapClient";
 import AuthProvider from "@/provider/AuthProvider";
+import { ReactQueryClientProvider } from "@/components/ReactQueryClient/ReactQueryClient";
 
 export const metadata: Metadata = {
   title: "Sistema-estoque",
@@ -18,9 +19,11 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <AuthProvider>
-          <Navbar />
-          {children}
-          <BootstrapClient />
+          <ReactQueryClientProvider>
+            <Navbar />
+            {children}
+            <BootstrapClient />
+          </ReactQueryClientProvider>
         </AuthProvider>
       </body>
     </html>
