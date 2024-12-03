a9da3fe361a429e60b5c2a3dd74636e96abc60c6
lazaro <marqueslazaro13@gmail.com>
2024-12-03 10:54:33 -0400

    pequena alteração no router de equipamento

--- backend/src/resources/equipamento/equipamento.router.ts
+++ backend/src/resources/equipamento/equipamento.router.ts
@@ -5,20 +5,20 @@ import schemaEquipamento from './equipamento.schema';
 import validate from '../../middlewares/validate';
 import isAdmin from '../../middlewares/isAdmin';
 
-router.get('/equipamento/', equipamentoController.index);
+router.get('/', equipamentoController.index);
 router.post(
-  '/equipamento/',
+  '/',
   isAdmin,
   validate(schemaEquipamento),
   equipamentoController.create,
 );
-router.get('/equipamento/:id', equipamentoController.read);
+router.get('/:id', equipamentoController.read);
 router.put(
-  '/equipamento/:id',
+  '/:id',
   isAdmin,
   validate(schemaEquipamento),
   equipamentoController.update,
 );
-router.delete('/equipamento/:id', isAdmin, equipamentoController.remove);
+router.delete('/:id', isAdmin, equipamentoController.remove);
 
 export default router;
